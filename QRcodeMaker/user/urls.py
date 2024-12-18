6cb37dede71948a3dd72f377293d7f3c19846041
Nikita Naumenko <solncevnikita80@gmail.com>
2024-12-18 21:35:19 +0200

    Styles for registration and login has been anded

--- QRcodeMaker/user/urls.py
+++ QRcodeMaker/user/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import render_registation
+
+from .views import render_registation, render_login
 
 urlpatterns = [
-    path('registration/', render_registation),
+    path('registration/', render_registation, name = "reg"),
+    path('login/', render_login, name = "login")
 
 ]
--- QRcodeMaker/user/views.py
+++ QRcodeMaker/user/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def render_registation(request):
     return render(request=request,template_name="registration/registration.html")
+
+def render_login(request):
+    return render(request=request,template_name="login/login.html")
