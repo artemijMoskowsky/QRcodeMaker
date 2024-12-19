d9561395dfd14219cb05d92a37d5725e0d829c10
Matvii Sivaiev <138612027+MatviiSivaiev2009@users.noreply.github.com>
2024-12-19 20:26:06 +0700

    adding contact page and design for contacts and home page

--- QRcodeMaker/QRcodeMaker/settings.py
+++ QRcodeMaker/QRcodeMaker/settings.py
@@ -123,3 +123,12 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'amoskowsky@gmail.com'  
+EMAIL_HOST_PASSWORD = 'thmq khjg wknh qats' 
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
