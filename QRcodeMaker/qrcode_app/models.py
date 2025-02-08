aaa2db9b34b1c10275acd6842b5f6b1f3f4f6b30
Julia Ovcharenko <samsungyuliyu@gmail.com>
2025-02-08 15:23:38 +0200

    Adding qr code to the database

--- QRcodeMaker/qrcode_app/models.py
+++ QRcodeMaker/qrcode_app/models.py
@@ -4,6 +4,7 @@ from user.models import CustomUser
 class CreateQr(models.Model):
     id = models.IntegerField(primary_key=True)
     image = models.ImageField(upload_to='images')
-    author_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     date = models.DateField()
+    link = models.TextField()
     
--- QRcodeMaker/qrcode_app/views.py
+++ QRcodeMaker/qrcode_app/views.py
@@ -4,20 +4,33 @@ import time
 import qrcode
 import os
 from .models import CreateQr
+import io
+from django.core.files.base import ContentFile
+import datetime
 
 
 # Create your views here.
 def render_create_qrcode(request):
+    ing_name = None
     if request.method == "POST":
-        my_qrcode = request.POST.get('data')
-        print(my_qrcode)
-        
-        img = qrcode.make(my_qrcode)
-        img_name = 'qrcode' + str(time.time()) + '.png'
-        img_path = os.path.join(settings.MEDIA_ROOT, img_name)
-        img.save(img_path)
-        
-        return render(request, 'create_qrcode/create_qrcode.html', {'img_name': img_name})
+        if request.user.is_authenticated:
+            my_qrcode = request.POST.get('data')
+            if my_qrcode:
+                print(my_qrcode)
+                
+                img = qrcode.make(my_qrcode)
+                img_name = 'qrcode' + str(time.time()) + '.png'
+                # img_path = os.path.join(settings.MEDIA_ROOT, img_name)
+                qrcode_io = io.BytesIO()
+                img.save(qrcode_io, format='PNG')
+
+                add_qrcode = CreateQr.objects.create(
+                    image=ContentFile(qrcode_io.getvalue(), name = img_name),
+                    link = my_qrcode,
+                    author_id = request.user,
+                    date = datetime.datetime.today())
+
+            return render(request, 'create_qrcode/create_qrcode.html', {'img_name': img_name})
 
     return render(request=request, template_name='create_qrcode/create_qrcode.html')
 def render_my_qrcodes(request):
