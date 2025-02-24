fe578f7914603004a6915a14accd7278b095425e
Matvii Sivaiev <138612027+MatviiSivaiev2009@users.noreply.github.com>
2025-02-24 20:49:00 +0200

    redirect on another page

--- QRcodeMaker/core/views.py
+++ QRcodeMaker/core/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 from QRcodeMaker.settings import EMAIL_HOST_USER
 from django.http import HttpResponse
 
 # Create your views here.
 def render_home(request):
-    if request.method == "POST" and request.user.is_authenticated:
-        license = request.POST.get("subscribe")
-        request.user.licence = license
+    if request.method == "POST":
+        createQR = request.POST.get("create-QR")
+        print(createQR, 1)
+        if createQR:
+           return redirect("qrcodes/create_qrcode/")
+        if request.user.is_authenticated:
+            license = request.POST.get("subscribe")
+            request.user.licence = license
     return render(request = request, template_name = "core.html")
 
 def contacts(request):
@@ -18,3 +23,5 @@ def contacts(request):
         recipient = 'artemij.mosckowsky.01062008@gmail.com'
         send_mail("Зворотній зв'язок",f"{name} написав відгук:\n{problem}\n\nПошта для зворотнього зв'язку {email}",EMAIL_HOST_USER,[recipient],fail_silently=False,) 
     return render(request, 'contacts.html')
+
+
