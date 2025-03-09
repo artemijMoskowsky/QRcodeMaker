d452f62f5305d78020bca01b01ea8ab8bf05b456
Nikita <143237900+Naumenko0Nikita@users.noreply.github.com>
2025-03-09 18:59:13 +0100

    Update models.py

--- QRcodeMaker/user/models.py
+++ QRcodeMaker/user/models.py
@@ -1,17 +1,13 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
+from django.db import models  # Импортируем модуль для работы с моделями Django
+from django.contrib.auth.models import AbstractUser  # Импортируем абстрактную модель пользователя Django
+from datetime import datetime  # Импортируем модуль работы с датой и временем
+from dateutil.relativedelta import relativedelta  # Импортируем функцию для работы с относительными датами
 # Create your models here.
 
-# Створюємо власний класс користувача
-class CustomUser(AbstractUser):
-    # Створюємо поле для ліцензії
-    licence = models.CharField(max_length=255, blank=True, null=True)
-    # Створюємо поле дати завершення ліцензії
-    licence_date = models.DateTimeField(default=datetime.now() + relativedelta(months=1))
 
-    # Задаємо стандартне строкове відображення
-    def __str__(self):
-        # Повертаємо ім'я користувача
-        return self.username
+class CustomUser(AbstractUser):  # Создаем кастомную модель пользователя, наследуясь от AbstractUser
+    licence = models.CharField(max_length=255, blank=True, null=True)  # Поле для хранения лицензии, может быть пустым
+    licence_date = models.DateTimeField(default=datetime.now() + relativedelta(months=1))  # Поле даты лицензии, по умолчанию +1 месяц от текущей даты
+    
+    def __str__(self):  # Определяем строковое представление объекта
+        return self.username  # Возвращаем имя пользователя
