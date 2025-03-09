f14a8aead7f48365ffddbe5b5b64ba3110b3dd2f
Nikita <143237900+Naumenko0Nikita@users.noreply.github.com>
2025-03-09 18:58:48 +0100

    Update urls.py

--- QRcodeMaker/user/urls.py
+++ QRcodeMaker/user/urls.py
@@ -1,12 +1,10 @@
-from django.urls import path
+from django.urls import path  # Импортируем функцию path для определения маршрутов
+from .views import render_registation, render_login, render_logout, render_account  # Импортируем представления из views.py
 
-from .views import render_registation, render_login, render_logout, render_account
 
-# Задаємо функції відображення для endpoint-ів
 urlpatterns = [
-    path('registration/', render_registation, name = "reg"),
-    path('login/', render_login, name = "login"),
-    path('logout/', render_logout, name = 'logout'),
-    path('account/', render_account, name = "account")
-
+    path('registration/', render_registation, name="reg"),  # URL для страницы регистрации
+    path('login/', render_login, name="login"),  # URL для страницы входа
+    path('logout/', render_logout, name='logout'),  # URL для выхода из системы
+    path('account/', render_account, name="account")  # URL для страницы аккаунта
 ]
